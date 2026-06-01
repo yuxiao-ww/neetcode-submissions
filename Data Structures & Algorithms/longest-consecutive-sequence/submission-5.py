444c9a373d96a706d370aaa26cca36bf7915ea1d
Yuxiao Wang <87699764+yuxiao-ww@users.noreply.github.com>
2026-06-01 15:21:13 -0500

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.py
@@ -0,0 +1,65 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        nums = list(set(nums))
+        nums.sort()
+        i, j = 0, 1
+        res, length = 0, 1
+
+        while i < len(nums) and j < len(nums):
+            if nums[j] == nums[j-1] + 1:
+                length += 1
+                j += 1
+            else:
+                res = max(length, res)
+                i = j
+                j = i + 1
+                length = 1
+        res = max(length, res)
+        return res
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        nums = list(set(nums))
+        nums.sort()
+
+        res, length = 0, 1
+        i, j = 0, 1
+        while i < len(nums) and j < len(nums):
+            if nums[j] == nums[j - 1] + 1:
+                length += 1
+                j += 1
+            else:
+                res = max(res, length)
+                i = j
+                j = i + 1
+                length = 1
+        res = max(res, length)
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
