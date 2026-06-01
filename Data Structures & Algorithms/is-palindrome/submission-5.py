1b5582ffcf8d76058b9c426378f68fbd410b2ddd
Yuxiao Wang <87699764+yuxiao-ww@users.noreply.github.com>
2026-06-01 15:26:40 -0500

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,48 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        valid = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890"
+        i, j = 0, len(s)-1
+
+        while i <= j:
+            if s[i] in valid and s[j] in valid:
+                if s[i].lower() != s[j].lower():
+                    return False
+                i += 1
+                j -= 1
+            elif s[i] not in valid:
+                i += 1
+            elif s[j] not in valid:
+                j -= 1
+        return True
+
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        valid = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890"
+        i, j = 0, len(s) - 1
+
+        while i <= j:
+            if s[i] not in valid:
+                i += 1
+            elif s[j] not in valid:
+                j -= 1
+            else:
+                if s[i].lower() != s[j].lower():
+                    return False
+                i += 1
+                j -= 1
+        return True
+        
+
+
+
+
+
+
+
+
+
+
+
+
