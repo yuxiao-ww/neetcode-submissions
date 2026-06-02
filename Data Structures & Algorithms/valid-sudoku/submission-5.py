f0ab871ab96ca5fda042bdddf995181e8e481d63
Yuxiao Wang <87699764+yuxiao-ww@users.noreply.github.com>
2026-06-02 14:11:10 -0500

    Add: valid-sudoku - submission-5

---
+++ Data Structures & Algorithms/valid-sudoku/submission-5.py
@@ -0,0 +1,75 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = [set() for _ in range(9)]
+        col = [set() for _ in range(9)]
+        box = [[set() for _ in range(3)] for _ in range(3)]
+
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                if num == '.':
+                    continue
+                if num in row[i] or num in col[j] or num in box[i//3][j//3]:
+                    return False
+                row[i].add(num)
+                col[j].add(num)
+                box[i//3][j//3].add(num)
+        
+        return True
+
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = [set() for _ in range(9)]
+        col = [set() for _ in range(9)]
+        box = [[set() for _ in range(3)] for _ in range(3)]
+
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                if num == '.':
+                    continue
+                if num in row[i] or num in col[j] or num in box[i//3][j//3]:
+                    return False
+                row[i].add(num)
+                col[j].add(num)
+                box[i//3][j//3].add(num)
+        return True
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = [set() for _ in range(9)]
+        col = [set() for _ in range(9)]
+        box = [[set() for _ in range(3)] for _ in range(3)]
+
+        for i in range(9):
+            for j in range(9):
+                ch = board[i][j]
+                if ch == '.':
+                    continue
+                if ch in row[i] or ch in col[j] or ch in box[i//3][j//3]:
+                    return False
+                row[i].add(ch)
+                col[j].add(ch)
+                box[i//3][j//3].add(ch)
+        
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
