b01434f93fedd8575d65827feb6e4a6a8772f839
Yuxiao Wang <87699764+yuxiao-ww@users.noreply.github.com>
2026-06-04 14:43:25 -0500

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.py
@@ -0,0 +1,19 @@
+# 单调栈
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        res = 0
+        prefix, postfix = [0] * len(height), [0] * len(height)
+        prefix[0] = height[0]
+        postfix[-1] = height[-1]
+
+        for i in range(1, len(height)):
+            prefix[i] = max(prefix[i - 1], height[i])
+        for i in range(len(height) - 2, -1, -1):
+            postfix[i] = max(postfix[i + 1], height[i])
+        
+        for i in range(len(height)):
+            res += min(prefix[i], postfix[i]) - height[i]
+        
+        return res
+        
+        
