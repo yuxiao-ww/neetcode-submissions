6ad630a44eb770114da6d89d7a7f9c395192631d
Yuxiao Wang <87699764+yuxiao-ww@users.noreply.github.com>
2026-06-06 18:30:55 -0500

    Add: trapping-rain-water - submission-10

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-10.py
@@ -0,0 +1,70 @@
+# 双指针
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        l, r = 0, len(height) - 1
+        maxL, maxR = height[0], height[-1]
+        res = 0
+        while l < r:
+            if maxL < maxR:
+                l += 1
+                maxL = max(maxL, height[l])
+                res += maxL - height[l]
+            else:
+                r -= 1
+                maxR = max(maxR, height[r])
+                res += maxR - height[r]
+        return res
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        maxl, maxr = height[0], height[-1]
+        res = 0
+        while l < r:
+            if maxl < maxr:
+                l += 1
+                maxl = max(maxl, height[l])
+                res += maxl - height[l]
+            else:
+                r -= 1
+                maxr = max(maxr, height[r])
+                res += maxr - height[r]
+        return res
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        prefix, postfix = [0] * len(height), [0] * len(height)
+        res = 0
+
+        prefix[0] = height[0]
+        postfix[-1] = height[-1]
+
+        for i in range(1, len(height)):
+            prefix[i] = max(prefix[i - 1], height[i])
+        for i in range(len(height)-2, -1, -1):
+            postfix[i] = max(postfix[i + 1], height[i])
+        
+        for i in range(len(height)):
+            res += min(prefix[i], postfix[i]) - height[i]
+        
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
