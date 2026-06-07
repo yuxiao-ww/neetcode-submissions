cb7c26c3373ee7c7438d0171dac3858042b4be9d
Yuxiao Wang <87699764+yuxiao-ww@users.noreply.github.com>
2026-06-07 00:06:32 -0500

    Add: is-palindrome - submission-12

---
+++ Data Structures & Algorithms/is-palindrome/submission-12.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        valid = "1234567890QAZWSXEDCRFVTGBYHNUJMIKOLPqazwsxedcrfvtgbyhnujmikolp"
+        l, r = 0, len(s) - 1
+        while l <= r:
+            if s[l] not in valid:
+                l += 1
+            elif s[r] not in valid:
+                r -= 1
+            else:
+                if s[l].lower() != s[r].lower():
+                    return False
+                l += 1
+                r -= 1
+        return True
