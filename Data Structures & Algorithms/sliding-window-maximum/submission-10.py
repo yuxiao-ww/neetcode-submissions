dcf8fc3950ade0d3b26b5189e00ffd44415ccfb0
Yuxiao Wang <87699764+yuxiao-ww@users.noreply.github.com>
2026-06-08 17:33:54 -0500

    Add: sliding-window-maximum - submission-10

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-10.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        queue = collections.deque()
+        l, r = 0, 0
+
+        for i, x in enumerate(nums):
+            # queue insert from right
+            while queue and x > nums[queue[-1]]:
+                queue.pop()
+            queue.append(i)
+
+            # queue semove from left
+            if i - queue[0] + 1 > k:
+                queue.popleft()
+
+            # record answer
+            if i >= k - 1:
+                res.append(nums[queue[0]])
+        
+        return res
