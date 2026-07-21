5dd4128df13c91c066c694ba7294feb7a8bc2f8b
Yuxiao Wang <87699764+yuxiao-ww@users.noreply.github.com>
2026-07-21 18:05:20 -0500

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i, num in enumerate(nums):
+            if num in dic:
+                return [dic[num], i]
+            else:
+                dic[target - num] = i
